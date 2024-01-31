124fbcdf0ff0a205b905e8e76621ee9e85e720ae
ManujDhull <manujdhull801@gmail.com>
2024-01-31 15:05:52 +0530

    Adding new attribute email

--- src/databases/migrations/2024.01.30T03.25.46.Create-New-Attribute-Email.ts
+++ src/databases/migrations/2024.01.30T03.25.46.Create-New-Attribute-Email.ts
@@ -2,18 +2,11 @@ import sequelize, { DataTypes } from 'sequelize';
 import type { Migration } from 'umzug';
 
 export const up: Migration = async ({ context: sequelize }) => {
-    await sequelize.getQueryInterface().addColumn(
-        'Users',
-        'Email',
-        {
-            type: DataTypes.STRING,
-            defaultValue: null
-        }
-    )
+  await sequelize.getQueryInterface().addColumn('Users', 'Email', {
+    type: DataTypes.STRING,
+    defaultValue: null,
+  });
 };
 export const down: Migration = async ({ context: sequelize }) => {
-    await sequelize.getQueryInterface().removeColoum(
-        'Users',
-        'Email'
-    );
+  await sequelize.getQueryInterface().removeColoum('Users', 'Email');
 };
