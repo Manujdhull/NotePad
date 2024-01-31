d6311cb87086258614771fe140743549f2c5c73b
ManujDhull <manujdhull801@gmail.com>
2024-01-31 15:11:14 +0530

    Adding new dto of email

--- src/users/dtos/users.email.dto.ts
+++ src/users/dtos/users.email.dto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 export class UserDtoEmail {
-    // @IsNotEmpty()
-    @IsString()
-    public Email: string;
+  // @IsNotEmpty()
+  @IsString()
+  public Email: string;
 }
