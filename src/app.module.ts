fa56fc3a6d7b2e259ee219978029d56f6887048e
ManujDhull <manujdhull801@gmail.com>
2024-01-31 15:02:38 +0530

    Adding MailModule & EnvConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,46 +10,22 @@ import { NoteModel } from './databases/models/note.model';
 import { SharedNoteModel } from './databases/models/shared-Notes.model';
 import { ShareController } from './sharing-notes/controllers/sharing-notes.controller';
 import { ShareModule } from './sharing-notes/sharing-notes.module';
-// import {ShareService} from './sharing-notes/services/sharing-notes.service'
 import { JwtService } from '@nestjs/jwt';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { MailModule } from './mail/mail.module';
+import { DatabaseModule } from './databases/database.module';
+import { EnvConfigModule } from './env-config/env-config.module';
 
 @Module({
   imports: [
     UsersModule,
     NotesModule,
     AuthModule,
-    SequelizeModule.forRoot({
-      // username: process.env.username,
-      // password: process.env.password,
-      // database: process.env.database,
-      username: 'mackmanuj',
-      password: 'Rubi@123',
-      database: 'NotePad',
-      dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      models: [UserModel, NoteModel, SharedNoteModel],
-    }),
     ShareModule,
-    MailerModule.forRoot({
-      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
-      defaults: {
-        from: '"nest-modules" <modules@nestjs.com>',
-      },
-      template: {
-        dir: __dirname + '/templates',
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
-        },
-      },
-    }),
     MailModule,
+    EnvConfigModule,
+    DatabaseModule,
   ],
   controllers: [AppController, ShareController],
   providers: [AppService, JwtService],
 })
-export class AppModule { }
+export class AppModule {}
