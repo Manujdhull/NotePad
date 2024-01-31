528e6425a37b251854df04b4f360439d6718a807
ManujDhull <manujdhull801@gmail.com>
2024-01-31 15:09:46 +0530

    setup for title,body,email,username

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -3,16 +3,20 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class MailService {
   constructor(private mailerService: MailerService) {}
-
-  async sendDeleteNoteMsg(title: string, body: string, email: string) {
-
+  async sendDeleteNoteMsg(
+    title: string,
+    body: string,
+    email: string,
+    username: string,
+  ) {
     await this.mailerService.sendMail({
       to: email,
       subject: 'Notes Update',
-      template: './mailTemplate',
+      template: './confirmation',
       context: {
         title: title,
-        body: body
+        body: body,
+        username: username,
       },
     });
   }
