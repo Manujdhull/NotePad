c6ac0566f37086a7f7226f50ec3495351c20603a
ManujDhull <manujdhull801@gmail.com>
2024-02-01 18:01:24 +0530

    setup image with multer

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -13,6 +13,8 @@ import {
   Render,
   UseGuards,
   Redirect,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { UserDtoSignUp } from '../dtos/users.signup.dto';
@@ -22,11 +24,38 @@ import { AuthGuard } from 'src/authentication/guard/auth.guard';
 import { ApiTags } from '@nestjs/swagger';
 import { UserDtoEmail } from '../dtos/users.email.dto';
 import { AuthUser } from '../authUser.decorator';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+// import multer, { diskStorage } from 'multer';
+const multer = require('multer');
+import path, { extname } from 'path';
+import { diskStorage } from 'multer';
+// import { memoryStorage } from 'multer'
+
+const storage=diskStorage({
+  destination: './uploads/',
+  filename: (req, file, cb) => {
+    var randomName = Array(32).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
+    console.log(randomName);
+    cb(null, `${randomName}${extname(file.originalname)}`)
+  }
+})
+// middleware
+// const Storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'Profiles')
+//   },
+//   filename(req, file, cb) {
+//     console.log(file);
+//     cb(null, Date.now() + path.extname(file.originalname))
+//   },
+// })
+
+// const upload= multer({storage:Storage})
 
 @ApiTags('user')
 @Controller('user')
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService) { }
   // (transform:means structuring data as same of our dto)
   // whitelist true means ignoring extra data
   /**
@@ -45,22 +74,37 @@ export class UsersController {
 
   // @UsePipes(new ValidationPipe({ transform: true, whitelist: true }))
   @UseGuards(AuthGuard)
-  @Get('verifyEmail')
-  @Render('verifyEmail')
+  @Get('profile')
+  @Render('profile')
   public async showEmailDisplay() {
     console.log('my Email in hbs verifying email');
   }
 
-  // @UsePipes(new ValidationPipe({ transform: true, whitelist: true }))
+
+  @UsePipes(new ValidationPipe({ transform: true, whitelist: true }))
   @UseGuards(AuthGuard)
-  @Post('verifyEmail')
+  @Post('profile')
+  @UseInterceptors(FileInterceptor('file', {storage: storage})
+  )
   @Redirect('/notes')
   public async verifyEmail(
     @AuthUser() authUser: UserModel,
     @Body() userDtoEmail: UserDtoEmail,
-  ) {
+    @UploadedFile() file
+  ): Promise<void> {
     console.log('my Email in hbs verifying email', userDtoEmail.Email);
-    this.usersService.createUserEmail(userDtoEmail.Email, authUser.id);
+    console.log("file we want to upload is this ", file.path)
+    const profilePicture=String(file.path);
+    console.log("file we want to upload is this ", profilePicture)
+    if(!userDtoEmail.Email){
+      this.usersService.createUserProfile( authUser.id,profilePicture);
+    }
+    else if(!(file)){
+      this.usersService.createUserEmail(userDtoEmail.Email,authUser.id)
+    }
+    else{
+      this.usersService.createUserEmailProfile(userDtoEmail.Email, authUser.id,profilePicture);
+    }
   }
 
   /**
