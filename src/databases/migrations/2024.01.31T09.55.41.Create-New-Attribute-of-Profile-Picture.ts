552e5d6b8b350cf0ce656831ff19288e0a4bdcbe
ManujDhull <manujdhull801@gmail.com>
2024-02-01 17:58:58 +0530

    Adding Profile picture Attribute

---
+++ src/databases/migrations/2024.01.31T09.55.41.Create-New-Attribute-of-Profile-Picture.ts
@@ -0,0 +1,12 @@
+import sequelize, { DataTypes } from 'sequelize';
+import type { Migration } from 'umzug';
+
+export const up: Migration = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().addColumn('Users', 'profilePicture', {
+    type: DataTypes.STRING,
+    defaultValue: null,
+  });
+};
+export const down: Migration = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().removeColumn('Users', 'profilePicture');
+};
