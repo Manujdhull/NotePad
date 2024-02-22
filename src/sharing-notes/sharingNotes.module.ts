797ef8b28417df379b5a9fba22346fd65f068a4a
ManujDhull <manujdhull801@gmail.com>
2024-02-22 17:22:48 +0530

    removing ForFeature from here now model of share is global

--- src/sharing-notes/sharingNotes.module.ts
+++ src/sharing-notes/sharingNotes.module.ts
@@ -1,23 +1,26 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { SharedNoteModel } from '../databases/models/shared-Notes.model';
+// import { SequelizeModule } from '@nestjs/sequelize';
+// import { SharedNoteModel } from '../databases/models/shared-Notes.model';
 import { ShareController } from './controllers/sharing-notes.controller';
 import { ShareService } from './services/sharing-notes.service';
-import { NoteModel } from '../databases/models/note.model';
+// import { NoteModel } from '../databases/models/note.model';
 import { AuthModule } from 'src/authentication/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { UserRepoModule } from 'src/users/user-repo.module';
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([SharedNoteModel]),
-    SequelizeModule.forFeature([NoteModel]),
+    // SequelizeModule.forFeature([SharedNoteModel]),
+    // SequelizeModule.forFeature([NoteModel]),
     AuthModule,
     JwtModule,
     UserRepoModule,
   ],
   controllers: [ShareController],
   providers: [ShareService],
-  exports: [SequelizeModule, ShareService],
+  exports: [
+    // SequelizeModule,
+    ShareService,
+  ],
 })
 export class SharingNotesModule {}
