4531257f10a5cc9cbf72b28cf8d364e804ae6101
ManujDhull <manujdhull801@gmail.com>
2024-02-22 17:22:21 +0530

    removing ForFeature from here now model of note is global

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { NotesController } from './controllers/notes.controller';
 import { NotesService } from './services/notes.service';
-import { NoteModel } from '../databases/models/note.model';
-import { SequelizeModule } from '@nestjs/sequelize';
+// import { NoteModel } from '../databases/models/note.model';
+// import { SequelizeModule } from '@nestjs/sequelize';
 import { AuthModule } from 'src/authentication/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { UserRepoModule } from 'src/users/user-repo.module';
@@ -11,7 +11,7 @@ import { MailModule } from 'src/mail/mail.module';
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([NoteModel]),
+    // SequelizeModule.forFeature([NoteModel]),
     AuthModule,
     JwtModule,
     UserRepoModule,
@@ -20,6 +20,9 @@ import { MailModule } from 'src/mail/mail.module';
   ],
   controllers: [NotesController],
   providers: [NotesService],
-  exports: [NotesService, SequelizeModule],
+  exports: [
+    NotesService,
+    //  SequelizeModule
+  ],
 })
 export class NotesModule {}
