975a1620aeb3911c3c67bfa6feaf20e88d519dd2
ManujDhull <manujdhull801@gmail.com>
2024-02-22 17:13:12 +0530

    Adding formating on file only changes of formatting

--- umzug.ts
+++ umzug.ts
@@ -1,26 +1,28 @@
 // import {  } from 'umzug';
-const umzug =require('umzug');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const umzug = require('umzug');
 import { Sequelize } from 'sequelize';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 const sequelize = new Sequelize(
-	process.env.database_name,
-	process.env.database_username,
-	process.env.database_password,
-	{
-	host:"localhost",
-	dialect: 'mysql'
-});
+  process.env.database_name,
+  process.env.database_username,
+  process.env.database_password,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+);
 
 export const migrator = new umzug.Umzug({
-	migrations: {
-		glob: ['src/databases/migrations/*.ts', { cwd: __dirname }],
-	},
-	context: sequelize,
-	storage: new umzug.SequelizeStorage({
-		sequelize,
-	}),
+  migrations: {
+    glob: ['src/databases/migrations/*.ts', { cwd: __dirname }],
+  },
+  context: sequelize,
+  storage: new umzug.SequelizeStorage({
+    sequelize,
+  }),
 });
 
-export type Migration =  (typeof migrator._types.migration);
+export type Migration = typeof migrator._types.migration;
